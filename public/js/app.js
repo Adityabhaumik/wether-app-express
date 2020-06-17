96e913fe78777bb5fd4114362c38a5168aea8816
Adityabhaumik <adi123bhaumik@gmail.com>
2020-06-17 16:42:27 +0530

    heroku compatibility2

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,6 @@ const wetherform = document.querySelector('form')
 const userlocation = document.querySelector('input')
 const message1 = document.querySelector('#message1')
 const message2 = document.querySelector('#message2')
-
-
 wetherform.addEventListener('submit', (e) =>{
     e.preventDefault()
         fetch('http://api.weatherstack.com/current?access_key=92c2da8d24bb7097b0f9fa6595eaebcb&query='+userlocation.value).then((response) =>{
@@ -23,6 +21,6 @@ wetherform.addEventListener('submit', (e) =>{
        
     })
 })
-    
-   
+      
 })
+//helloworld
